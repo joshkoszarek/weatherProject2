37ff129f78e62e882726f337d9aff42ad95c1fe1
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com>
2025-02-24 13:12:37 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# webpack-setup
+Setup files with correct loaders
+
+
+npm init -y
+npm install webpack webpack-cli --save-dev
+npm install --save-dev style-loader css-loader
+npm install --save-dev html-webpack-plugin
+npm install --save-dev webpack-dev-server
+npm install -D babel-loader @babel/core @babel/preset-env webpack
+npm install --save-dev jest
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+      test: /\.(?:js|mjs|cjs)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }]
+          ]
+        }
+      }
+    }
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+  devtool: 'inline-source-map',
+  /*  optimization: { 
+        runtimeChunk: 'single',
+    }, */
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+};
