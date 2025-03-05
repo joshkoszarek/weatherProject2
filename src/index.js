ca65f0d496344a5592b2c88cb1a8a48357b93025
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com>
2025-03-05 10:04:18 -0600

    Add the ability to the essential weather information.
    Now, you can get a nice object ready to be displayed. Previously, the entire object given from an API call was used. Now, a filtered version of the data is created, only containing the necessary information to be displayed.

--- src/index.js
+++ src/index.js
@@ -60,3 +60,124 @@ function getNumberFromString(str) {
 }
 
 addCarouselSliderFunctionality();
+
+async function getWeatherData(city, state) {
+  //make an API call to the Visual Crossing API
+  //convert the json to an object
+  // return the data as a js object
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}%2C%20${state}?unitGroup=us&key=2B8BEFV7ZKPJSVHZRL5ALNRP5&contentType=json`,
+    { mode: 'cors' }
+  );
+  const weatherData = await response.json();
+
+  return weatherData;
+}
+
+//need to have multiple async functions
+
+// const waterfordWeather = getWeatherData('Waterford', 'WI');
+
+// console.log(waterfordWeather);
+
+//make an API call
+//return the data object
+//take the data object and construct a new object
+//with what is needed for the display
+// update the display using the object
+
+async function addSearchFunctionality(city, state) {
+  const allWeatherData = await getWeatherData(city, state);
+  const necessaryWeatherData = filterWeatherData(allWeatherData);
+  console.log(necessaryWeatherData);
+  // displayWeatherData(necessaryWeatherData);
+}
+
+function filterWeatherData(allWeatherData) {
+  const address = allWeatherData.address;
+  const rightNowWeatherObject = getRightNowWeather(allWeatherData);
+  const hourlyWeatherArrOfObjects = getHourlyWeather(allWeatherData);
+  const weeklyWeatherArrOfObjects = getWeeklyWeather(allWeatherData);
+
+  const filteredWeatherObject = {
+    dayOverview: rightNowWeatherObject,
+    hourly: hourlyWeatherArrOfObjects,
+    weekly: weeklyWeatherArrOfObjects,
+  };
+  return filteredWeatherObject;
+}
+
+function getRightNowWeather(allWeatherDataObj) {
+  const currentDay = allWeatherDataObj.days[0];
+  const rightNowWeatherObject = {
+    tempMax: currentDay.tempmax,
+    tempMin: currentDay.tempmin,
+    temp: currentDay.temp,
+    feelsLike: currentDay.feelslike,
+    icon: currentDay.icon,
+  };
+  return rightNowWeatherObject;
+}
+
+function getHoursLeftInDay() {
+  const now = new Date();
+  const currentHour = now.getHours();
+  const hoursLeftInDay = 24 - currentHour;
+  return hoursLeftInDay;
+}
+
+function getCurrentHour() {
+  const now = new Date();
+  const currentHour = now.getHours();
+  return currentHour;
+}
+function getHourlyWeather(allWeatherDataObj) {
+  const currentDayHourlyArray = allWeatherDataObj.days[0].hours;
+  const nextDayHourlyArray = allWeatherDataObj.days[1].hours;
+  const currentHour = getCurrentHour();
+  const hoursLeftInDay = getHoursLeftInDay();
+
+  const hourlyWeatherArray = [];
+  for (let i = 0; i < 24; i++) {
+    if (hoursLeftInDay > i) {
+      const oneHourObj = {
+        currentTemp: currentDayHourlyArray[i + currentHour].temp,
+        icon: currentDayHourlyArray[i + currentHour].icon,
+        precipitationProb: currentDayHourlyArray[i + currentHour].precipprob,
+        time: currentDayHourlyArray[i + currentHour].datetime,
+      };
+      hourlyWeatherArray.push(oneHourObj);
+    } else {
+      const oneHourObj = {
+        currentTemp: nextDayHourlyArray[i - hoursLeftInDay].temp,
+        icon: nextDayHourlyArray[i - hoursLeftInDay].icon,
+        precipitationProb: nextDayHourlyArray[i - hoursLeftInDay].precipprob,
+        time: nextDayHourlyArray[i - hoursLeftInDay].datetime,
+      };
+      hourlyWeatherArray.push(oneHourObj);
+    }
+  }
+  return hourlyWeatherArray;
+}
+
+function getWeeklyWeather(allWeatherDataObj) {
+  const weeklyWeather = [];
+  for (let i = 0; i < 7; i++) {
+    const dailyWeather = allWeatherDataObj.days[i];
+    const dailyWeatherObject = {
+      highTemp: dailyWeather.tempmax,
+      lowTemp: dailyWeather.tempmin,
+      icon: dailyWeather.icon,
+      precipProb: dailyWeather.precipprob,
+    };
+    weeklyWeather.push(dailyWeatherObject);
+  }
+  return weeklyWeather;
+}
+
+function displayWeatherData(necessaryWeatherData) {
+  // get all the query selectors
+  // fill them with the right info
+}
+
+addSearchFunctionality('Waterford', 'WI');
