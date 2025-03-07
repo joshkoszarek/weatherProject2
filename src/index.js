cbb163176a7479b4d2a3c9f9ae11d8f7d2e09dff
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com>
2025-03-06 21:28:39 -0600

    Add search functionality
    Now a user can search for a location's weather. Error handling and sanitization of the form data still needs to be added.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { displayWeather } from './search.js';
+import { addSearchBarFunctionality } from './search.js';
 import { addUnitToggleButtonFunctionality } from './unitToggle.js';
 // import './style.css';
 function main() {
   // buildBoilerPlate();
   //something to do with Local Storage adding saved cities
   //local storage has saved cities and currently viewing city
-  displayWeather('Waterford, WI');
+  addSearchBarFunctionality();
   addUnitToggleButtonFunctionality();
 }
 
--- src/search.js
+++ src/search.js
@@ -1,20 +1,18 @@
 import { getWeatherData, filterWeatherData } from './dataFiltering.js';
 import { displayWeatherData } from './display.js';
-export { displayWeather };
+export { addSearchBarFunctionality };
 async function displayWeather(location) {
   const allWeatherData = await getWeatherData(location);
   const necessaryWeatherData = filterWeatherData(allWeatherData);
-  //   console.log(necessaryWeatherData);
   displayWeatherData(necessaryWeatherData);
 }
 
-const searchBarForm = document.querySelector('.search-bar-container form');
-searchBarForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const inputData = e.target.elements.searchBar.value;
-  console.log(inputData);
-  //get search input into correct form for an API call
-  // display weather with location as argument
-
-  e.target.elements.searchBar.value = '';
-});
+function addSearchBarFunctionality() {
+  const searchBarForm = document.querySelector('.search-bar-container form');
+  searchBarForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputData = e.target.elements.searchBar.value;
+    displayWeather(inputData);
+    e.target.elements.searchBar.value = '';
+  });
+}
