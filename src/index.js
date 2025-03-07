8cb63ce9ea61c7cd0eb1243d321285d8f6c4fc0b
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com>
2025-03-07 10:16:16 -0600

    Delete unnecessary comments

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import { addUnitToggleButtonFunctionality } from './unitToggle.js';
 import { buildBoilerPlate, displayWeather } from './display.js';
 // import './style.css';
 function main() {
-  // buildBoilerPlate();
-  //something to do with Local Storage adding saved cities
-  //local storage has saved cities and currently viewing city
   buildBoilerPlate();
   displayWeather('Waterford, WI');
   addSearchBarFunctionality();
